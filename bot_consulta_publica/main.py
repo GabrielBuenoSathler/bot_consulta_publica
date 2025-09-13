e550a9fe09c642614408602142bb21b22fdf0019
gabriel <gabrielbuenosathler@gmail.com>
2025-09-13 18:21:47 -0400

    adicionando o ignore

--- bot_consulta_publica/main.py
+++ bot_consulta_publica/main.py
@@ -1,4 +1,12 @@
+import os
+from dotenv import load_dotenv
+
+
 def main():
+    load_dotenv()  # take variables from .env.
+    my_var = os.getenv("MY_VAR")
     print("rodando meu codigo")
+    print(f"MY_VAR = {my_var}")
+
 
 main()
