65c4bd310ca1b8b260bd05a174394c91f8ba013f
gabriel <gabrielbuenosathler@gmail.com>
2025-09-14 17:39:20 -0400

    test main

--- bot_consulta_publica/main.py
+++ bot_consulta_publica/main.py
@@ -1,33 +1,75 @@
+#!/usr/bin/env python
+# pylint: disable=unused-argument
+# This program is dedicated to the public domain under the CC0 license.
+
+"""
+Simple Bot to reply to Telegram messages.
+
+First, a few handler functions are defined. Then, those functions are passed to
+the Application and registered at their respective places.
+Then, the bot is started and runs until we press Ctrl-C on the command line.
+
+Usage:
+Basic Echobot example, repeats messages.
+Press Ctrl-C on the command line or send a signal to the process to stop the
+bot.
+"""
+
+import logging
+
+from telegram import ForceReply, Update
+from telegram.ext import Application, CommandHandler, ContextTypes, MessageHandler, filters
 import os
-import requests
 from dotenv import load_dotenv
-import os
-import re
+# Enable logging
+logging.basicConfig(
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
+)
+# set higher logging level for httpx to avoid all GET and POST requests being logged
+logging.getLogger("httpx").setLevel(logging.WARNING)
+
+logger = logging.getLogger(__name__)
 
 
-def verifica_cnpj (cnpj):
-    cnpj = str(cnpj)
-    if re.search(r'^[0-9]{14}$|^[0-9]{2}\.[0-9]{3}\.[0-9]{3}/[0-9]{4}-[0-9]{2}$',cnpj):
-        print("formatacao correta")
-    else:
-        print("formatacao incorreta ")
-
-def conect_api(cnpj):
-    verifica_cnpj(cnpj)
-    r = requests.get(f"https://brasilapi.com.br/api/cnpj/v1/{cnpj}")
-    if r.status_code == 200:
-      # for values  in r.json():
-      #     print(f"{values}")
-      response_dict = r.json()
-      print(len(response_dict.values()))
-      print(f"cep: {response_dict['cep']}")
-
-def main():
-    
-    load_dotenv()  # take variables from .env.
-    db_url = os.getenv("TELEGRAM_KEY")
-    print(db_url)
-    conect_api(19131243000197)
+# Define a few command handlers. These usually take the two arguments update and
+# context.
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """Send a message when the command /start is issued."""
+    user = update.effective_user
+    await update.message.reply_html(
+        rf"Hi {user.mention_html()}!",
+        reply_markup=ForceReply(selective=True),
+    )
 
 
-main()
+async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """Send a message when the command /help is issued."""
+    await update.message.reply_text("Help!")
+
+
+async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """Echo the user message."""
+    await update.message.reply_text(update.message.text)
+
+
+def main() -> None:
+    """Start the bot."""
+    # Create the Application and pass it your bot's token.
+    load_dotenv()
+
+    TOKEN =  os.getenv("TOKEN")
+    application = Application.builder().token(TOKEN).build()
+
+    # on different commands - answer in Telegram
+    application.add_handler(CommandHandler("start", start))
+    application.add_handler(CommandHandler("help", help_command))
+    application.add_handler(CommandHandler("echo", echo))
+    # on non command i.e message - echo the message on Telegram
+    application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, echo))
+
+    # Run the bot until the user presses Ctrl-C
+    application.run_polling(allowed_updates=Update.ALL_TYPES)
+
+
+if __name__ == "__main__":
+    main()
